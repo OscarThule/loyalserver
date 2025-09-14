89ed1f183349b9fc142b2bc33676896d34e26314
ThuleOscar <lekgorothuleoscar@gmail.com>
2025-09-14 16:45:40 +0200

    Fix import paths for Render deployment

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,5 +1,5 @@
-import Post from '../models/Post.js';
-import User from '../models/User.js';
+import Post from './models/Post.js';
+import User from './models/User.js';
 import mongoose from 'mongoose';
 import path from 'path';
 import fs from 'fs';
