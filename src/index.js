dfc35fd756c3245525ca1e563e887dc9edd4ee84
ThuleOscar <lekgorothuleoscar@gmail.com>
2025-09-15 09:31:10 +0200

    i have used module alias on my paths

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+import 'module-alias/register';
+
 import http from 'http';
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { networkInterfaces } from 'os';
-import connectDB from './config/db.js';
-import authRoutes from './routes/auth.routes.js';
-import userRoutes from './routes/user.routes.js';
-import postRoutes from './routes/post.routes.js';
+import connectDB from '@config/db.js';
+import authRoutes from '@routes/auth.routes.js';
+import userRoutes from '@routes/user.routes.js';
+import postRoutes from '@routes/post.routes.js';
 import morgan from 'morgan'; // Added for request logging
 import path from 'path';
 
